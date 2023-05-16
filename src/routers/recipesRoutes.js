ab33cd6baa3b9e9ddc27b3e7e99c27353b06fa68
Izaan Saqib <saqibiza@corp.contoursoftware.net>
2023-05-16 20:07:28 +0500

    fs crud completed

--- src/routers/recipesRoutes.js
+++ src/routers/recipesRoutes.js
@@ -3,9 +3,11 @@ const {getAll,save,update,remove}=require("../controllers/recipesController")
 
 const router=express.Router();
 
-router.get('/',getAll)
-router.post('/',save)
-router.put("/:id",update)
-router.delete("/:id",remove)
+// router.get('/',getAll)
+// router.post('/',save)
+// router.put("/:id",update)
+// router.delete("/:id",remove)
 
+router.route("/").get(getAll).post(save)
+router.route("/:id").put(update).delete(remove)
 module.exports=router
